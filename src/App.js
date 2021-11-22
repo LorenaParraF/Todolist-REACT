5bb8192d8d7571d285f8d401b1eec6b3a08d7721
lore198429 <lore198429@gmail.com>
2021-11-22 16:36:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+//Components
+import Header from "./components/Header";
+import Loader from "./components/Loader";
+
+import Todo from "./components/Todo";
+
+//Styles
+import "./styles/App.css";
+import "./styles/Buttons.css";
+
+const App = () => {
+  // states
+  const [todoList, setTodoList] = useState([]);
+
+  // efect
+  useEffect(() => {
+    const handleTodoList = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+      const result = await response.json();
+      const resultTodoList = result.slice(0, 20);
+      setTodoList(resultTodoList);
+    };
+    handleTodoList();
+  }, []);
+
+  const handleCompleteTodo = (id) => {
+    setTodoList(
+      todoList.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const complete = todoList.filter((item) => !item.completed);
+
+  const incomplete = todoList.filter((item) => !!item.completed);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <>
+        <div className="Buttons">
+          <li className="button">
+            {" "}
+            <button onClick={() => {}}>Todas {todoList.length}</button>
+          </li>
+          <li>
+            <button onClick={() => {}}>Completados {complete.length}</button>
+          </li>
+          <li>
+            <button onClick={() => {}}>Incompletos {incomplete.length}</button>
+          </li>
+        </div>
+      </>
+
+      <div className="todo-container">
+        {todoList.map((singleTodo) => (
+          <Todo
+            key={singleTodo.id}
+            title={singleTodo.title}
+            status={singleTodo.completed}
+            handleCompleteTodo={handleCompleteTodo}
+            id={singleTodo.id}
+          />
+        ))}
+        : (
+        <Loader />)
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+//Estilos
+
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h4 className="logo">TODO LIST</h4>
+    </header>
+  );
+};
+
+export default Header;
